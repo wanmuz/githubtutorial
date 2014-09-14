42d3231fa5f0a1128f73ba9f3adf280c9ffc0d1f
Wanmuz <wanmuz86@gmail.com>
2014-09-14 21:48:09 +0800

    Add log message on app launch Add button that logs to the console when tapped.

--- GitUseExample/ViewController.h
+++ GitUseExample/ViewController.h
@@ -10,6 +10,7 @@
 
 @interface ViewController : UIViewController
 
+- (IBAction)buttonPressed:(id)sender;
 
 @end
 
--- GitUseExample/ViewController.m
+++ GitUseExample/ViewController.m
@@ -24,4 +24,8 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)buttonPressed:(id)sender {
+    
+    NSLog(@"Button is pressed");
+}
 @end
